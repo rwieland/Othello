c215a5ab665f27b9c95524ee31458a73d94e3972
Robert Wieland <rwieland92@gmail.com>
2017-07-31 13:38:22 -0400

    Added parse function to parse stat_str

--- js/stats.js
+++ js/stats.js
@@ -1,7 +1,17 @@
 var GameStats = function() {
-	this.stat_string = ''
+	this.stat_str = ''
+	this.data = []
 }
 
 GameStats.prototype.write = function(str) {
-	this.stat_string += str + '\n'
+	this.stat_str += str + '\n'
+	this.parse()
+}
+
+GameStats.prototype.parse = function() {
+	var lines = this.stat_str.split('\n')
+	lines = lines.slice(0, lines.length - 1)
+	this.data = lines.map(function(x) {
+		return x.split(',')
+	})
 }
